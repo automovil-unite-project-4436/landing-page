11afa9ce6b936e3caf10a0549ffba7c81783136f
Loberos <rengiforandy11@gmail.com>
2025-03-28 19:50:19 -0500

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import {
 
 @Component({
   selector: 'app-root',
-  imports: [TranslateModule, HomePageComponent, Button, NavbarComponent, FunctionalitiesPageComponent],
+  imports: [TranslateModule, HomePageComponent, NavbarComponent, FunctionalitiesPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
